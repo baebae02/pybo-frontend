971d2cc80d9d630fb0d1c802068cc98e06b8c324
Seo Hyeon Bae <bae4614@gmail.com>
2022-06-07 20:47:21 +0900

    feat: app-bar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-      </div>
-
+    <v-app-bar absolute dense color="#FFFFFF" class="flex text-center justify-center align-center">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-title>BAE!</v-app-bar-title>
       <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
-
     <v-main>
       <Intro/>
       <router-view :key="$route.fullPath"/>
--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
-    <v-text-field
-        solo
-        label="Your Name Here"
-        clearable
-    ></v-text-field>
-    <button @click="getQuestion">BUTTON</button>
-    <ul v-for="question in this.questions.question" :key="question.id">
-      <li>subject: {{ question.subject}}</li>
-      <li>content: {{ question.content}}</li>
-    </ul>
+<!--    <v-text-field-->
+<!--        solo-->
+<!--        label="Your Name Here"-->
+<!--        clearable-->
+<!--    ></v-text-field>-->
+<!--    <button @click="getQuestion">BUTTON</button>-->
+<!--    <ul v-for="question in this.questions.question" :key="question.id">-->
+<!--      <li>subject: {{ question.subject}}</li>-->
+<!--      <li>content: {{ question.content}}</li>-->
+<!--    </ul>-->
+    <div class="topText">
+      <h1>Welcome to BaeBae!</h1>
+      <p>안녕하세요, 초보 개발자 배서현입니다.<br>재능보단 노력이 저의 무기입니다😆</p>
+    </div>
   </div>
 </template>
 
@@ -42,5 +46,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.topText {
+  display: flex;
+  background-color: antiquewhite;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 80px 0px;
+  padding: 20px;
 }
 </style>
