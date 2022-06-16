a0e60344334f01c3c3bab8a12ae32ddd1fb14522
baebae02 <bae4614@gmail.com>
2022-06-16 05:34:33 +0000

    feat: Text component

---
+++ src/components/TextOption.vue
@@ -0,0 +1,50 @@
+<template>
+    <span>
+      <span v-if="PurPle" :class="{PurPle}">
+        {{ content }}
+        </span>
+      <div v-else :class="{Bold, Gray, General}">
+        {{ content }}
+      </div>
+    </span>
+    
+</template>
+
+<script>
+export default {
+  name: 'TextOption',
+  data() {
+    return {
+      
+    };
+  },
+  props: [
+      'content',
+      'Bold',
+      'Gray',
+      'General',
+      'PurPle',
+  ],
+}
+</script>
+
+<style>
+    .Bold {
+        font-weight: 700;
+        font-size: 48px;
+        color: #3E3E3E;
+    }
+    .Gray {
+        font-weight: 700;
+        font-size: 24px;
+        color: #808080;
+    }
+    .General {
+        font-size: 21px;
+        font-weight: 500;
+    }
+    .PurPle {
+        color: #744CF3;
+        font-weight: 700;
+    }
+</style>
