7f2c1f8b525fe4f4c854f99807350f14387e1aae
Seo Hyeon Bae <bae4614@gmail.com>
2022-06-19 17:08:41 +0900

    feat: router

--- src/components/GNB.vue
+++ src/components/GNB.vue
@@ -1,30 +1,37 @@
 <template>
     <div class="gnb">
-		<img src="@/assets/logo.svg">
-		<div class="right">
-			<div :class="{text: true, now: true}">
-				소개
-			</div>
-			<div :class="{text: true, now}">
-				스터디
-			</div>
-			<div :class="{text: true, now}">
-				과제
-			</div>
-			<div :class="{text: true, now}">
-				출석부
-			</div>
-			<div class="sign">
-				로그인
-			</div>
-		</div>
+      <img src="@/assets/logo.svg" alt="" @click="goHome">
+      <div class="right">
+        <router-link :class="{text: true, now: true}" to="/">
+          소개
+        </router-link>
+        <router-link :class="{text: true, now}" to="/study">
+          스터디
+        </router-link>
+        <router-link :class="{text: true, now}" to="/">
+          과제
+        </router-link>
+        <router-link :class="{text: true, now}" to="/">
+          출석부
+        </router-link>
+        <router-link class="sign" to="/signup">
+          로그인
+        </router-link>
+      </div>
 	</div>
 </template>
 
 <script>
+import router from "@/router";
+
 export default {
   name: 'GNB',
   components: {
+  },
+  methods: {
+    goHome() {
+      router.push({ path: '/' })
+    }
   },
   data: () => ({
     //
@@ -33,19 +40,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  a {
+    text-decoration: none;
+  }
 	.gnb {
 		position: fixed;
 		top: 0;
 		left: 0;
 		right: 0;
-		
+
 		z-index: 100;
-		
+
 		display: flex;
 		flex-direction: row;
 		align-items: center;
 		justify-content: space-between;
-		
+
 		background-color: #754ff3e6;
 		backdrop-filter: blur(4px);
 		padding: 0 40px;
@@ -64,9 +74,9 @@ export default {
 				font-weight: bold;
 				cursor: pointer;
 				padding: 10px 10px;
-				
+
 				user-select: none;
-				
+
 				&:hover {
 					color: #e5e5e5;
 				}
@@ -91,9 +101,9 @@ export default {
 				border-radius: 8px;
 				cursor: pointer;
 				padding: 8px 16px;
-				
+
 				user-select: none;
-				
+
 				&:hover {
 					color: #ffffff;
 					border: solid 2px #ffffff00;
@@ -103,7 +113,7 @@ export default {
 					color: #E5E5E5;
 					border: solid 2px #ffffff00;
 					background-color: #AD9DE0;
-					
+
 				}
 			}
 		}
