e618de19f067ae8cce3df834a3a34d07cae6fb1b
Seo Hyeon Bae <bae4614@gmail.com>
2022-07-06 23:36:50 +0900

    feat: yarn build

---
+++ dist/js/logout.f9484cfc.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["logout"],{"6f8d":function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"logout"})},u=[],o=n("a18c"),s={name:"Logout",data:function(){return{}},created:function(){this.$store.state.username="",o["a"].push({path:"/"})}},c=s,r=n("2877"),l=Object(r["a"])(c,a,u,!1,null,null,null);e["default"]=l.exports}}]);
+//# sourceMappingURL=logout.f9484cfc.js.map
